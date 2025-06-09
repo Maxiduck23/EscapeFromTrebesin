cda803ae573664e32299c39817bbe9d475b6bab4
simo4ka <maksympolyvoda@gmail.com>
2025-06-09 22:07:10 +0200

    base2

--- booking/urls.py
+++ booking/urls.py
@@ -1,16 +1,9 @@
-# escapefromtrebesin/booking/urls.py
-
 from django.urls import path
 from . import views
 
+app_name = 'booking'
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('kontact/', views.kontact, name='kontact'),
-    path('details/', views.details, name='details'),
-    path('tomb/', views.tomb, name='tomb'),
-    path('lab/', views.lab, name='lab'),
-    path('alcatraz/', views.alcatraz, name='alcatraz'),
-    path('booking/', views.booking_view, name='booking'),
+    path('', views.booking_view, name='booking'),
     path('ajax/get_available_slots/', views.get_available_slots, name='get_available_slots'),
 ]
